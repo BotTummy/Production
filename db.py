5461672ff73f16daff527c546fde1bad3b55289a
BotTummy <ng.waritthorn@gmail.com>
2024-10-16 17:06:00 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def db_connection():
     conn = mysql.connector.connect(
-        host='75f8c2ff60c3',
-        # host='localhost',
+        # host='75f8c2ff60c3',
+        host='localhost',
         user='root',
         password='Isylzjko0',
         database='production'
